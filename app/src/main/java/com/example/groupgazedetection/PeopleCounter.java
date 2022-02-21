f309dc0568a2e6d5539518b727eb4adc3adca452
Ian Kerr <s22ian@test.com>
2022-02-21 13:57:16 -0500

    Initial Android Project Commit

---
+++ app/src/main/java/com/example/groupgazedetection/PeopleCounter.java
@@ -0,0 +1,8 @@
+package com.example.groupgazedetection;
+
+import org.opencv.android.*;
+import org.opencv.objdetect.HOGDescriptor;
+
+public class PeopleCounter {
+    HOGDescriptor hog = new HOGDescriptor();
+}
