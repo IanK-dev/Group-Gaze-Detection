031b4a290742b6ddc4486e34db4d49579357580a
Ian Kerr <s22ian@test.com>
2022-03-06 09:15:05 -0500

    Made dummy buttons work

--- app/src/main/java/com/example/groupgazedetection/HomeActivity.java
+++ app/src/main/java/com/example/groupgazedetection/HomeActivity.java
@@ -2,7 +2,9 @@ package com.example.groupgazedetection;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -10,5 +12,25 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+    }
+
+    public void gotoLiveDetection(View view){
+        Intent intent = new Intent(this, LiveDetectionActivity.class);
+        startActivity(intent);
+    }
+
+    public void gotoSingleImage(View view){
+        Intent intent = new Intent(this, SingleImageActivity.class);
+        startActivity(intent);
+    }
+
+    public void gotoMultiImage(View view){
+        Intent intent = new Intent(this, MultiImageActivity.class);
+        startActivity(intent);
+    }
+
+    public void gotoSettings(View view){
+        Intent intent = new Intent(this, SettingsActivity.class);
+        startActivity(intent);
     }
 }
