4e454fc036732f9fcc5bf221b4f4267f478a521f
GarJar <joshgarjar@gmail.com>
2022-03-27 19:29:16 -0400

    Started implementation of multi image detection. Changed app icon and added icon to splash screen.

--- app/src/main/java/com/example/groupgazedetection/MultiImageActivity.java
+++ app/src/main/java/com/example/groupgazedetection/MultiImageActivity.java
@@ -2,13 +2,98 @@ package com.example.groupgazedetection;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ContentResolver;
+import android.content.ClipData;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageSwitcher;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.widget.ViewSwitcher;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MultiImageActivity extends AppCompatActivity {
+
+    Button select;
+    ImageSwitcher imageView;
+    int PICK_IMAGE_MULTIPLE = 1;
+        //String imageEncoded;
+    TextView total;
+    ArrayList<Uri> mArrayUri;
+    int position = 0;
+    List<String> imagesEncodedList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_multi_image);
+        select = findViewById(R.id.select);
+        total = findViewById(R.id.text);
+        imageView = findViewById(R.id.image);
+            //previous = findViewById(R.id.previous);
+        mArrayUri = new ArrayList<Uri>();
+
+        // showing all images in imageswitcher
+        imageView.setFactory(new ViewSwitcher.ViewFactory() {
+            @Override
+            public View makeView() {
+                ImageView imageView1 = new ImageView(getApplicationContext());
+                return imageView1;
+            }
+        });
+
+        imageView = findViewById(R.id.image);
+
+        // click here to select image
+        select.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                // initialising intent
+                Intent intent = new Intent();
+
+                // setting type to select to be image
+                intent.setType("image/*");
+
+                // allowing multiple image to be selected
+                intent.putExtra(Intent.EXTRA_ALLOW_MULTIPLE, true);
+                intent.setAction(Intent.ACTION_GET_CONTENT);
+                startActivityForResult(Intent.createChooser(intent, "Select Picture"), PICK_IMAGE_MULTIPLE);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        // When an Image is picked
+        if (requestCode == PICK_IMAGE_MULTIPLE && resultCode == RESULT_OK && null != data) {
+            // Get the Image from data
+            if (data.getClipData() != null) {
+                ClipData mClipData = data.getClipData();
+                int cout = data.getClipData().getItemCount();
+                for (int i = 0; i < cout; i++) {
+                    // adding imageuri in array
+                    Uri imageurl = data.getClipData().getItemAt(i).getUri();
+                    mArrayUri.add(imageurl);
+                }
+                // setting 1st selected image into image switcher
+                imageView.setImageURI(mArrayUri.get(0));
+                position = 0;
+            } else {
+                Uri imageurl = data.getData();
+                mArrayUri.add(imageurl);
+                imageView.setImageURI(mArrayUri.get(0));
+                position = 0;
+            }
+        } else {
+            // show this if no image is selected
+            Toast.makeText(this, "You haven't picked Image", Toast.LENGTH_LONG).show();
+        }
     }
 }
