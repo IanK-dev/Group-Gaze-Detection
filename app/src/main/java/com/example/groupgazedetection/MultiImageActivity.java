aadaeb1adc66031e80e079374c7ef61588daada6
ChefDeash <deshawnhayes16@gmail.com>
2022-03-28 01:09:32 -0400

    Made Proper settings, Connected the settings to each section. Moved settings button on home screen to show that most settings effect live detection as of now.

--- app/src/main/java/com/example/groupgazedetection/MultiImageActivity.java
+++ app/src/main/java/com/example/groupgazedetection/MultiImageActivity.java
@@ -96,4 +96,10 @@ public class MultiImageActivity extends AppCompatActivity {
             Toast.makeText(this, "You haven't picked Image", Toast.LENGTH_LONG).show();
         }
     }
+
+    public void gotoSettings(View view){
+        Intent intent = new Intent(this, SettingsActivity.class);
+        startActivity(intent);
+    }
+
 }
