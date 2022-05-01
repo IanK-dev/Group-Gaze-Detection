a53095507bef1a901d8c0fcdc745c84e7f62a472
Ian Kerr <ikerr5555@gmail.com>
2022-04-30 21:21:02 -0400

    Added swipe interaction helper class

--- app/src/main/java/com/example/groupgazedetection/VideoAnalysisActivity.java
+++ app/src/main/java/com/example/groupgazedetection/VideoAnalysisActivity.java
@@ -2,9 +2,14 @@ package com.example.groupgazedetection;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.ImageView;
 
+import org.opencv.android.Utils;
 import org.opencv.core.Mat;
 
 import java.util.ArrayList;
@@ -12,7 +17,7 @@ import java.util.ArrayList;
 public class VideoAnalysisActivity extends AppCompatActivity {
     long[] frames = new long[50];
     ArrayList<Mat> convertedFrames = new ArrayList<Mat>();
-
+    ImageView frameDisplay;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,7 +32,24 @@ public class VideoAnalysisActivity extends AppCompatActivity {
             Log.d("LiveDetectionActivity", "Current Val: " + frames[frameIndex]);
             frameIndex = frameIndex + 1;
         }
-
         Log.d("LiveDetectionActivity", "Number of frames: " + convertedFrames.size());
+        frameDisplay = findViewById(R.id.capturedFrameView);
+        Bitmap thisMap = Bitmap.createBitmap(convertedFrames.get(0).width(), convertedFrames.get(0).height(), Bitmap.Config.ARGB_8888);
+        Utils.matToBitmap(convertedFrames.get(0), thisMap);
+        frameDisplay.setImageBitmap(thisMap);
+        frameDisplay.setOnTouchListener(new swipeListener(this) {
+            public void onSwipeRight() {
+                Log.d("LiveDetectionActivity", "Swiped right");
+            }
+            public void onSwipeLeft() {
+                Log.d("LiveDetectionActivity", "Swiped left");
+            }
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                return gestureDetector.onTouchEvent(event);
+            }
+        });
+
+
     }
 }
